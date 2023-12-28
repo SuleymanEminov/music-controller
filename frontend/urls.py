bd4cc857e6463f8f83224f81d59d44670b7c9613
suleymaneminov <suleymaneminov07@gmail.com>
2023-12-28 16:27:48 -0500

    info page (functional component)

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,6 +5,7 @@ app_name = 'frontend'
 
 urlpatterns = [
     path('', views.index, name=''),
+    path('info', views.index, name='info'),
     path('join', views.index, name='join'),
     path('create', views.index, name='create'),
     path('room/<str:roomCode>', views.index, name='room')
